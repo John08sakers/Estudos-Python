4705860963fff8cd3ff873f4c156bf7f2d6294d1
Antonio Jonathan <john08.aj@gmail.com>
2024-10-02 09:07:28 -0300

    atualização

--- Exercicios/ex020.py
+++ Exercicios/ex020.py
@@ -5,4 +5,4 @@ a3 = input('Terceiro aluno: ')
 a4 = input('Quarto aluno: ')
 list = [ a1, a2, a3, a4]
 shuffle(list)
-print(f'A orde de apresentação será:\n {list}')
+print(f'A onde de apresentação será:\n {list}')
